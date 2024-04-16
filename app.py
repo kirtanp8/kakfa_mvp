39be4b79c51fbfd2cdf882b0debc6821d73d83d7
Kirtan <kirtanp8@icloud.com>
2024-04-16 16:22:06 +0100

    amended files

--- app.py
+++ app.py
@@ -7,6 +7,7 @@ app = Flask(__name__)
 def index():
     producer = KafkaProducer(bootstrap_servers='localhost:9092')
     producer.send('test-topic', b'Hello, Kafka!')
+    print("Message sent to Kafka topic: test-topic") 
     return 'Message sent to Kafka topic: test-topic'
 
 if __name__ == '__main__':
